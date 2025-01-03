73d1f3b3f6c5059083d4cebbc21c7ca675742e13
Adam Noffsinger <adam.noffsinger@gmail.com>
2025-01-03 08:39:56 -0800

    Commented out model changes for testflight push

--- Mettle/Models/WorkoutEntry.swift
+++ Mettle/Models/WorkoutEntry.swift
@@ -8,17 +8,17 @@
 import Foundation
 import CloudKit
 
-struct WorkoutEntry: Identifiable, Hashable {
-    let id = UUID()
-    let workoutType: WorkoutType
-    let date: Date
-    let score: TimeInterval // Time value for the workout score
-    let note: String?
-
-    init(workoutType: WorkoutType, date: Date, score: TimeInterval, note: String? = nil) {
-        self.liftType = liftType
-        self.date = date
-        self.score = score
-        self.note = note
-    }
-}
+//struct WorkoutEntry: Identifiable, Hashable {
+//    let id = UUID()
+//    let workoutType: WorkoutType
+//    let date: Date
+//    let score: TimeInterval // Time value for the workout score
+//    let note: String?
+//
+//    init(workoutType: WorkoutType, date: Date, score: TimeInterval, note: String? = nil) {
+//        self.liftType = liftType
+//        self.date = date
+//        self.score = score
+//        self.note = note
+//    }
+//}
